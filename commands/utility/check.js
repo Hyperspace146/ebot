5b03689ff40a03f54f16c8901cc46ef207e94ceb
Hyperspace146 <adriandinh146@gmail.com>
2025-06-11 15:08:52 -0700

    add inital check slash command

---
+++ commands/utility/check.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+        .setName('check')
+        .setDescription('checks personal e counter'),
+	async execute(interaction) {
+		await interaction.reply('idk yet');
+	},
+};
