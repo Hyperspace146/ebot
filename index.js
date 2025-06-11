80ff033ceadf6403961a14a11736d3ec04f04a26
Hyperspace146 <adriandinh146@gmail.com>
2025-06-11 15:30:14 -0700

    move events to events dir

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { token } = require('./config.json');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// Create collection of slash commands by dynamically retrieving command files
+// Create client-accessible collection of slash commands by dynamically retrieving command files
 client.commands = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
@@ -26,36 +26,6 @@ for (const folder of commandFolders) {
 		}
 	}
 }
-
-// Set up listener to call corresponding slash command based on interaction's commandName
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-	
-    const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		}
-	}
-});
-
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
 
 // Log in to Discord with your client's token
 client.login(token);
