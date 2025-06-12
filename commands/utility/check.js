eb92e242e4f54d57706ad485f0953752286a53cc
Hyperspace146 <adriandinh146@gmail.com>
2025-06-11 17:52:12 -0700

    modify check to use getECounter

--- commands/utility/check.js
+++ commands/utility/check.js
@@ -5,6 +5,6 @@ module.exports = {
         .setName('check')
         .setDescription('checks personal e counter'),
 	async execute(interaction) {
-		await interaction.reply(String(interaction.client.database.increaseECounter(interaction.user.username, 1)));
+		await interaction.reply(String(interaction.client.database.getECounter(interaction.user.username)));
 	},
 };
