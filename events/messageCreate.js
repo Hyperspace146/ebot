43201941a3d23ce51d3c218aa973b15ed12fe2ba
Hyperspace146 <adriandinh146@gmail.com>
2025-06-12 22:07:04 -0700

    timeout user when sending word without e (not sure if functional)

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,17 +1,29 @@
-const { Events } = require('discord.js');
+const { Events, PermissionsBitField  } = require('discord.js');
+
+const eOnlyChannels = ["e", "test"];
+const atToken = new RegExp('<@[0-9]+>');
 
 // Set up listener to for all messages
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(interaction) {
+        // Ignore messages from bots
+        if (interaction.author.bot) return;
+
         // Count number of words with e, then add to counter
         var numWordsWithE = 0;
         var tokens = interaction.content.split(" ");
         for (var token of tokens) {
             if (token.includes("e") || token.includes("E")) {
                 numWordsWithE++;
+            } else if (!atToken.test(token) && eOnlyChannels.includes(interaction.channel.name)) {
+                // If word doesn't contain e or E, time out the user
+                const user = await interaction.guild.members.fetch(interaction.author.id);
+                console.log(`timeout ${user.user.globalName} for word ${token}`);
+                await user.timeout(100_000, "hehe");
+                return;
             }
         }
-        await interaction.client.database.increaseECounter(interaction.author.username, numWordsWithE);
+        interaction.client.database.increaseECounter(interaction.author.username, numWordsWithE);
 	},
 };
