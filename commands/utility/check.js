b7eb5f52833d9d8898d6fbba17d79b32a7dc60e5
Hyperspace146 <adriandinh146@gmail.com>
2025-06-12 22:05:44 -0700

    update check command to set target and have name of user

--- commands/utility/check.js
+++ commands/utility/check.js
@@ -3,8 +3,14 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
         .setName('check')
-        .setDescription('checks personal e counter'),
+        .setDescription('checks someone\'s e counter')
+        .addUserOption(option => 
+            option.setName('target')
+                .setDescription('the user to check')
+                .setRequired(false)),
 	async execute(interaction) {
-		await interaction.reply(String(interaction.client.database.getECounter(interaction.user.username)));
+        const target = interaction.options.getUser('target');
+        var username = target ? target.username : interaction.user.username;
+		await interaction.reply(username + ": " + String(interaction.client.database.getECounter(username)));
 	},
 };
